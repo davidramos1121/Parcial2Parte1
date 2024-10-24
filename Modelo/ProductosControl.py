2ad73330bf321034b6d68d8e78e4b208e4de77e6
davidramos1121 <152838084+davidramos1121@users.noreply.github.com>
2024-10-24 15:00:16 -0500

    correccion de productosControl
    es hacer lo mismo con el resto de clases

---
+++ Modelo/ProductosControl.py
@@ -0,0 +1,56 @@
+class ProductosControl():
+    
+    def __init__(Self, nombreProducto,registroIca, frecuencia,precio):
+        Self.__nombreProducto =  nombreProducto
+        Self.__registroIca = registroIca
+        Self.__frecuencia = frecuencia
+        Self.__precio = precio
+        
+        
+    @property
+    def nombreProducto(Self):
+        return Self.__nombreProducto
+    
+    @nombreProducto.setter
+    def nombreProducto(Self,nombreProducto):
+         if not nombreProducto.strip():
+            raise ValueError("El nombre del producto no puede estar vacío.")
+        
+         Self.__nombreProducto = nombreProducto
+        
+    
+    @property
+    def registroIca(Self):
+        return Self.__registroIca
+    
+    @registroIca.setter 
+    def registroIca(Self, registroIca):
+        if len(registroIca.strip()) > 0:
+            Self.__registroIca = registroIca
+        else:
+            raise ValueError("El registro ICA no puede estar vacío.")
+        
+    
+    @property
+    def frecuencia(Self):
+        return Self.__frecuencia
+    
+    @frecuencia.setter
+    def frecuencia(Self, frecuencia):
+        if frecuencia > 0:
+            Self.__frecuencia = frecuencia
+        else: 
+            raise ValueError("Error en el valor de la frecuencia de aplicacion")
+        
+  
+    @property 
+    def precio(Self):
+        return Self.__precio
+    
+    @precio.setter
+    def precio(Self,precio):
+        if precio > 0:
+            Self.__precio = precio
+        else: 
+            raise ValueError("Error en el precio del producto") 
+        
